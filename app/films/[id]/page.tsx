c75266515df2f6513b82be7c56bcfbf462fab98f
gabesone <sena6.gabriel@gmail.com>
2024-03-19 16:08:43 -0300

    add type to films/[id]

--- app/films/[id]/page.tsx
+++ app/films/[id]/page.tsx
@@ -1,6 +1,23 @@
 import React from "react";
 
-export const page = (id: string) => {
-  const movie_id = id;
+type movie_id = {
+  id: number;
+  title: string;
+  vote_average: number;
+  vote_count: number;
+  popularity: number;
+  poster_path: string;
+  original_language: string;
+  original_title: string;
+  genre_ids: number[];
+  backdrop_path: string;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+};
+
+const page = ({ id }: movie_id) => {
   return <div>page</div>;
 };
+
+export default page;
