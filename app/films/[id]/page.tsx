c629bcafb78a92b5dd6ed45cf79247f2d18ceebc
gabesone <sena6.gabriel@gmail.com>
2024-03-19 16:02:58 -0300

    change type films/[id]

--- app/films/[id]/page.tsx
+++ app/films/[id]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
-export const page = ({ params }: { params: { id: string } }) => {
+export const page = (id: string) => {
+  const movie_id = id;
   return <div>page</div>;
 };
