1c5d7de1af7c2a4a4c8132f861ea03a425222aef
gabesone <sena6.gabriel@gmail.com>
2024-03-19 16:10:16 -0300

    change type

--- app/films/[id]/page.tsx
+++ app/films/[id]/page.tsx
@@ -1,22 +1,6 @@
 import React from "react";
 
-type movie_id = {
-  id: number;
-  title: string;
-  vote_average: number;
-  vote_count: number;
-  popularity: number;
-  poster_path: string;
-  original_language: string;
-  original_title: string;
-  genre_ids: number[];
-  backdrop_path: string;
-  adult: boolean;
-  overview: string;
-  release_date: string;
-};
-
-const page = ({ id }: movie_id) => {
+const page = (id: string) => {
   return <div>page</div>;
 };
 
